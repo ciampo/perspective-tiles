ebe8cd786b09e2e0b6c84b9a58d0818ccac74b27
Marco Ciampini <marco.ciampo@gmail.com>
2018-08-24 00:18:09 +0100

    chore(build)

--- dist/bundle.js
+++ dist/bundle.js
@@ -86,6 +86,8 @@ class Sketch {
 
     // Bind
     this.drawFrame = this.drawFrame.bind(this);
+    this.onPointerMove = this.onPointerMove.bind(this);
+    this.onPointerUp = this.onPointerUp.bind(this);
 
     // Rendering
     this._canvas = document.createElement('canvas');
@@ -95,7 +97,29 @@ class Sketch {
     // Device pixel ratio.
     this._DPR = 1;// window.devicePixelRatio;
 
+    this._baseUnitSize = 80;
+    this._colorBrightness = [0.9, 0.7, 0.5];
+    this._hue = Math.random();
+
+    this._root.addEventListener('pointermove', this.onPointerMove, false);
+    this._root.addEventListener('pointerup', this.onPointerUp, false);
+
     this.onResize();
+  }
+
+  onPointerUp() {
+    this._hue = Math.random();
+  }
+
+  onPointerMove(evt) {
+    const xPos = (evt.clientX - this._viewportSize.w / 2) /
+      (this._viewportSize.w);
+    const yPos = (evt.clientY - this._viewportSize.h / 2) /
+      (this._viewportSize.h);
+    const f = xPos / 4 + yPos / 4;
+
+    this._colorBrightness[1] = 0.5 - f;
+    this._colorBrightness[2] = 0.5 + f;
   }
 
   startDrawing() {
@@ -117,27 +141,97 @@ class Sketch {
     this._canvas.setAttribute('height', `${innerHeight * this._DPR}px`);
   }
 
+  _generateFillColor(brightness) {
+    const hue = Math.round(360 * this._hue);
+    return `hsl(${hue}, 78%, ${90 - Math.round((1 - brightness) * 40)}%)`;
+  }
+
+  _generateCanvasPattern(density, fill) {
+    const s = 48;
+    const c = document.createElement("canvas");
+    c.width = s;
+    c.height = s;
+    const ctx = c.getContext("2d");
+
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, s, s);
+
+    ctx.fillStyle = fill;
+
+    for (let x = 0; x <= s; x += density) {
+      for (let y = 0; y <= s; y += density) {
+        ctx.beginPath();
+        ctx.arc(x, y, 1, 0, 2 * Math.PI);
+        ctx.closePath();
+        ctx.fill();
+      }
+    }
+
+    // ctx.arc(50, 50, 10, 0, 2 * Math.PI);
+
+
+    return c;
+  }
+
   drawFrame() {
     if (this._drawing) {
       requestAnimationFrame(this.drawFrame);
     }
 
     // Draw
-    this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    // this._ctx.fillStyle = this._generateFillColor(this._colorBrightness[0], 0);
+    this._ctx.fillStyle = this._ctx.createPattern(
+      this._generateCanvasPattern(8, `hsl(${Math.round(this._hue * 360)}, 75%, 30%)`),
+      'repeat',
+    );
+    this._ctx.fillRect(0, 0, this._canvas.width, this._canvas.height);
 
-    const gridSize = {
-      x: 100,
-      y: 60,
-    };
+    for (let x = 0; x < this._viewportSize.w / this._baseUnitSize + 1; x += 1) {
+      for (let y = 0; y < this._viewportSize.h / this._baseUnitSize + 1; y += 1) {
+        const yOffset = x % 2 === 0 ? 0 : -0.5;
 
-    for (let x = 0; x < this._viewportSize.w; x += gridSize.x) {
-      for (let y = 0; y < this._viewportSize.h; y += gridSize.y) {
-        this._ctx.fillStyle = `rgba(0, 0, 0, ${Math.random()})`;
-        this._ctx.fillRect(x, y, gridSize.x, gridSize.y);
+        this._drawCell(x, y + yOffset);
       }
     }
+  }
 
-    // Update model
+  _drawCell(x, y) {
+
+    const u = this._baseUnitSize;
+
+    this._ctx.fillStyle = this._generateFillColor(this._colorBrightness[1], 1);
+
+    this._ctx.fillStyle = this._ctx.createPattern(
+      this._generateCanvasPattern(4, `hsl(${Math.round(this._hue * 360)}, 75%, 30%)`),
+      'repeat',
+    );
+
+    this._ctx.beginPath();
+    this._ctx.moveTo(x * u, y * u);
+    this._ctx.lineTo((x + 0.5) * u, y * u);
+    this._ctx.lineTo(x * u, (y + 0.5) * u);
+    this._ctx.moveTo((x + 1) * u, (y + 1) * u);
+    this._ctx.lineTo((x + 0.5) * u, (y + 1) * u);
+    this._ctx.lineTo((x + 1) * u, (y + 0.5) * u);
+    this._ctx.closePath();
+    this._ctx.fill();
+
+    this._ctx.fillStyle = this._generateFillColor(this._colorBrightness[2], 2);
+
+    this._ctx.fillStyle = this._ctx.createPattern(
+      this._generateCanvasPattern(3, `hsl(${Math.round(this._hue * 360)}, 75%, 30%)`),
+      'repeat',
+    );
+
+    this._ctx.beginPath();
+    this._ctx.moveTo((x + 1) * u, y * u);
+    this._ctx.lineTo((x + 1) * u, (y + 0.5) * u);
+    this._ctx.lineTo((x + 0.5) * u, y * u);
+    this._ctx.moveTo(x * u, (y + 1) * u);
+    this._ctx.lineTo((x + 0.5) * u, (y + 1) * u);
+    this._ctx.lineTo(x * u, (y + 0.5) * u);
+    this._ctx.closePath();
+    this._ctx.fill();
   }
 }
 /* harmony export (immutable) */ __webpack_exports__["a"] = Sketch;
